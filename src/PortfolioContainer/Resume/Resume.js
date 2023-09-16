46a12dad68e0ac121e5592fb8b0c13bb17a27132
SPDTeeKoo <107340539+SPDTeeKoo@users.noreply.github.com>
2023-09-16 13:10:14 +0530

    experience updated

--- src/PortfolioContainer/Resume/Resume.js
+++ src/PortfolioContainer/Resume/Resume.js
@@ -144,13 +144,15 @@ const Resume = (props) => {
         </div> */}
         <div className="experience-description">
           <span className="resume-description-text">
+            Took initiative to develop a web-based dashboard using React and
+            MySQL for monitoring and analyzing products and workers status data,
+            which significantly improved data accessibility and decisionmaking
+            processes. Design and created a sample management platform using
+            technologies like React, Node.js and Material UI. Helped evolve
+            front-end and back-end stack of company website infrastructure.
             Collaborated with cross-functional teams to design and optimize
             mechanical systems, enhancing product performance and reducing
-            production costs. Utilized programming skills to automate repetitive
-            tasks, increasing workflow efficiency. Took initiative to develop a
-            web-based dashboard for monitoring and analyzing products and
-            workers status data, which significantly improved data accessibility
-            and decision-making processes.
+            production costs.
           </span>
           {/* <br />
           <span className="resume-description-text">
